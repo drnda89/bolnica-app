f6d20ffd63cbbf37eeab66539d9ca3733e30a8a7
Bojan Drinic <drnda89@gmail.com>
2018-09-05 22:52:19 +0200

    bolnica

---
+++ src/app/doktori/doktori.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doktori',
+  templateUrl: './doktori.component.html',
+  styleUrls: ['./doktori.component.css']
+})
+export class DoktoriComponent implements OnInit {
+	
+	prikaz = false;
+	
+	ime = '';
+	ime3 = [];
+	doktor = {
+		ime1: '',
+		ime2: ''
+	};
+
+
+	
+	
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  dodaj(bla) {
+  	
+  	this.prikaz = true;
+  	this.ime = bla;
+  	this.ime3.push(this.doktor.ime1, this.doktor.ime2);
+  	if(this.doktor.ime1 === '' || this.doktor.ime2 === '') {
+  		this.prikaz = false;
+  		this.ime3.splice(2);
+  		alert('polje je prazno')
+  	 }
+  	 this.doktor.ime1 = '';
+  	 this.doktor.ime2 = '';
+  	
+  	console.log(bla.value);
+  	
+  		
+  }
+
+}
---
+++ src/app/pacijenti/pacijenti.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pacijenti',
+  templateUrl: './pacijenti.component.html',
+  styleUrls: ['./pacijenti.component.css']
+})
+export class PacijentiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
